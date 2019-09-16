ae78f14f5ca7e829781d0f3956494dc084063727
t0yohei <inuneko690121@gmail.com>
2019-09-16 19:23:21 +0900

    goのgrpcサーバーを呼び出すrubyの実装を追加

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
+require 'pinger_services_pb.rb'
+require 'pinger_pb.rb'
+
 class ApplicationController < ActionController::API
+  def ping
+    pinger_stub = Pinger::Pinger::Stub.new('localhost:5300', :this_channel_is_insecure)
+    pong = pinger_stub.ping(Pinger::Empty.new)
+    render json: {pong: pong.text}
+  end
 end
